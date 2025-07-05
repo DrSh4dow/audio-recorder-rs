ffbfdabe1d740dd5fa6f89fa0df6ae420b4ec80d
drsh4dow <daniel.morettiv@gmail.com>
2025-07-05 11:53:17 -0400

    added base tests

---
+++ tests/record_single.rs
@@ -0,0 +1,71 @@
+use recorder_core::Recorder;
+use tracing_test::traced_test;
+
+#[test]
+#[traced_test]
+fn record_input_only() {
+    tracing::info!("Spawning record input test");
+
+    tracing::info!("Creating recorder");
+    let mut recorder = Recorder::new();
+
+    tracing::info!("Starting recorder");
+    let receiver = match recorder.start(true) {
+        Ok(receiver) => receiver,
+        Err(e) => {
+            panic!("Failed to start recorder: {}", e);
+        }
+    };
+    tracing::info!("Recorder started");
+
+    tracing::info!("Asserting if is recording");
+    assert!(recorder.get_is_recording(), "Recorder is not recording");
+
+    let config = match recorder.get_config() {
+        Ok(c) => c,
+        Err(e) => {
+            tracing::error!("Failed to get config: {}", e);
+            panic!("Failed to get config");
+        }
+    };
+
+    let mut writer = hound::WavWriter::create(
+        "output.wav",
+        hound::WavSpec {
+            sample_rate: config.sample_rate,
+            channels: config.channels,
+            bits_per_sample: (config.sample_size * 8) as _,
+            sample_format: hound::SampleFormat::Float,
+        },
+    )
+    .expect("Could not generate writer");
+
+    tracing::info!("Awaiting data");
+    let instant = std::time::Instant::now();
+
+    while let Ok(d) = receiver.recv() {
+        if instant.elapsed().as_secs() > 2 {
+            break;
+        }
+
+        for sample in d {
+            writer.write_sample(sample).ok();
+        }
+    }
+    tracing::info!("Finished recording");
+    if let Err(e) = recorder.stop() {
+        tracing::error!("Failed to stop recorder: {}", e);
+        panic!("Failed to stop recorder: {}", e);
+    };
+
+    // assert output.wav exists
+    assert!(
+        std::path::Path::new("output.wav").exists(),
+        "output.wav does not exist"
+    );
+
+    // delete output.wav
+    if let Err(e) = std::fs::remove_file("output.wav") {
+        tracing::error!("Failed to delete output.wav: {}", e);
+    }
+}
